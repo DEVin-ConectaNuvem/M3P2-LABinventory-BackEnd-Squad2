fe8b81f05ed39b322fe797df7bbb8bdc048d1e44
Marcelo Coelho <92119579+MCoelho222@users.noreply.github.com>
2022-11-15 16:35:28 -0300

    feat: endpoint-post cadastro de colaborador

--- src/app/controllers/collaborator.py
+++ src/app/controllers/collaborator.py
@@ -1,10 +1,10 @@
-
 from flask import Blueprint
 from flask.wrappers import Response
-from src.app import mongo_client
+from flask import request, jsonify
 from bson import json_util
 from pymongo import ASCENDING, DESCENDING
-from flask import request, jsonify
+from src.app import mongo_client
+from src.app.middlewares.auth import required_fields, has_logged
 
 collabs = Blueprint("collabs", __name__,  url_prefix="/collabs")
 
@@ -17,13 +17,28 @@ def get_all_collabs():
     mimetype="application/json"
   )
 
-@collabs.route("/", methods=["POST"])
+@collabs.route("/cadastro", methods=["POST"])
+@has_logged()
+@required_fields(['nome', 'genero', 'nascimento', 'telefone', 'bairro', 'cargo', 'cep', 'email', 'localidade', 'logradouro', 'numero', 'uf'])
 def insert_collabs():
     collabs = request.get_json()
-
-    mongo_client.collabs.insert_one(collabs)
-
-    return {"sucesso": f"Colaborador inserido com sucesso"}, 201
+    print(collabs)
+    allCollabs = list(mongo_client.collabs.find())
+  
+    exists = False
+    checkEmail = collabs['email']
+    for collab in allCollabs:
+      if collab['email'] == checkEmail:
+        exists = True
+    if not exists:
+      collabs['id'] = len(allCollabs) + 1
+      mongo_client.collabs.insert_one(collabs)
+      return {"sucesso": "Colaborador cadastrado com sucesso."}, 201
+    if exists:
+      return {"erro": "Email já cadastrado."}, 401
+    else:
+      return {"erro": "Algo deu errado..."}, 500
+    
     
 @collabs.route("/", methods=["DELETE"])
 def delete_all():
