a271e88b1a92afad01f5cfd6ab683bbdba6dcfaf
mayconrcampos <maycon.campos@gmail.com>
2022-11-15 10:03:26 -0300

    C02 feat: fazendo login e gerando token

--- src/app/controllers/user.py
+++ src/app/controllers/user.py
@@ -3,8 +3,10 @@ from flask import Blueprint
 from flask.wrappers import Response
 from src.app import mongo_client
 from bson import json_util
-from pymongo import ASCENDING, DESCENDING
-from flask import request, jsonify
+#from pymongo import ASCENDING, DESCENDING
+from flask import request
+from src.app.utils import set_password, validate_password, generate_jwt
+from datetime import datetime, timedelta, timezone
 
 users = Blueprint("users", __name__,  url_prefix="/users")
 
@@ -21,8 +23,14 @@ def get_all_users():
 def insert_user():
     user = request.get_json()
 
-    mongo_client.users.insert_one(user)
+    payload = {
+      "name": user['name'],
+      "email": user["email"],
+      "password": set_password(user["password"])
+    }
 
+    mongo_client.users.insert_one(payload)
+    
     return {"sucesso": f"User inserido com sucesso"}, 201
     
 @users.route("/", methods=["DELETE"])
@@ -30,3 +38,29 @@ def delete_all():
     mongo_client.users.delete_many({})
 
     return {"sucesso": "Usuários limpos com sucesso"}, 200
+
+@users.route("/login", methods=["POST"])
+def login_user():
+    user_request = request.get_json()
+    
+    try:
+        user = mongo_client.users.find_one({"email" : user_request['email']})
+
+        print(user['password'], user['email'])
+
+        if not user or not validate_password(user['password'], user_request['password']):
+            return {"error": "Suas credenciais estão incorretas!", "status_code": 401}
+
+        payload = {
+            "name": user['name'],
+            "email": user['email'],
+            "exp": datetime.now(tz=timezone.utc) + timedelta(days=1),
+        }
+        token = generate_jwt(payload)
+
+        return {"token": token, "status_code": 200}
+
+    except Exception as e:
+        return {"error": f"{e}"}
+
+
