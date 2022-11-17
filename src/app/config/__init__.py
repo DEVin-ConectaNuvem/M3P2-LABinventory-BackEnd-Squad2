cda02278a0fb74ed3f1a82cd0093a619c5694b46
Marcelo Coelho <92119579+MCoelho222@users.noreply.github.com>
2022-11-17 20:54:12 -0300

    fix: MONGO_URI

--- src/app/config/__init__.py
+++ src/app/config/__init__.py
@@ -12,24 +12,27 @@ class Config:
     OAUTHLIB_INSECURE_TRANSPORT = os.getenv('OAUTHLIB_INSECURE_TRANSPORT')
     FRONTEND_URL = os.getenv("FRONTEND_URL")
     BACKEND_URL = os.getenv('BACKEND_URL')
-    MONGO_URI = os.getenv("MONGO_URI")
     FLASK_DEBUG = os.getenv("FLASK_DEBUG")
 
 class Development(Config):
     TESTING = False 
     DEBUG = True
+    MONGO_URI = "mongodb+srv://mayconrcampos:tZa66W2iubEEHDS@cluster0.fbj6hbc.mongodb.net/devinventory/?retryWrites=true&w=majority"
 
 class Production(Config):
     TESTING = False 
     DEBUG = False
+    MONGO_URI = "mongodb+srv://mayconrcampos:tZa66W2iubEEHDS@cluster0.fbj6hbc.mongodb.net/devinventory-prod/?retryWrites=true&w=majority"
    
 class Testing(Config):
     DEBUG = False
     TESTING = True
+    MONGO_URI = "mongodb+srv://mayconrcampos:tZa66W2iubEEHDS@cluster0.fbj6hbc.mongodb.net/devinventory-test/?retryWrites=true&w=majority"
 
 class Homologation(Config):
     DEBUG = False
     TESTING = False
+    MONGO_URI = "mongodb+srv://mayconrcampos:tZa66W2iubEEHDS@cluster0.fbj6hbc.mongodb.net/devinventory-homo/?retryWrites=true&w=majority"
 
 app_config = {
     "development": Development,
